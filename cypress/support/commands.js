dce5e9247c35e138483b6b8fdf9f93dc6eed044e
kol <kol3wang@gmail.com>
2024-11-04 21:14:48 -0500

    command to answer

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -5,3 +5,8 @@ Cypress.Commands.add('startQuiz', () => {
     cy.visit('/');
     cy.contains('Start Quiz').click(); // Assumes 'Start Quiz' button text exists
   });
+
+// Command to answer a question by selecting the first option
+Cypress.Commands.add('answerQuestion', () => {
+    cy.get('.answer-option').first().click(); // Update '.answer-option' to match your selector
+  });
